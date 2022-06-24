e7a2d9332ebcfcb786ec676b738a5370d215a684
Guillermo H leija <guillermo_leija@yahoo.com>
2022-06-23 20:56:05 -0500

    Métodos sort

--- Python/Semana2/Listas.py
+++ Python/Semana2/Listas.py
@@ -1,27 +1,27 @@
 ## ejemplo de listas en python
 
 #Creando una lista
-nombres = ["Paola", "Carlos", "Mario", "Israel", "Manuel", "Luis", "Guillermo"]
+#nombres = ["Paola", "Carlos", "Mario", "Israel", "Manuel", "Luis", "Guillermo"]
 
 # Accediendo y pintando un elemento con x posicion de la lista
-print(nombres[1])
+#print(nombres[1])
 
 # Cambiando un elemento con x posicion en la lista
-nombres[0] = "Elizabeth"
+#nombres[0] = "Elizabeth"
 
 # Agregar elementos a la lista
 
-nombres.insert(7, "Juan")
-nombres.insert(1, "Pedrita")
+#nombres.insert(7, "Juan")
+#nombres.insert(1, "Pedrita")
 
-nombres.append("Perenganita")
+#nombres.append("Perenganita")
 ##nombres.("sutanita")
 
-print(nombres)
+#print(nombres)
 
 # Despues de dos meses
 
-nombres.remove("Juan")
+#nombres.remove("Juan")
 
 
 #Barrer una lista
@@ -32,26 +32,63 @@ nombres.remove("Juan")
 #FOREACH
 
 
-for x in nombres:
-    print(x)
+#for x in nombres:
+#    print(x)
 
 
-nombres.pop(1)
+#nombres.pop(1)
 
-print(nombres)
-
-
-for y in range(1):
-    print(nombres[y])
-
-print(len(nombres))
+#print(nombres)
 
 
-nombres = ["Paola", "Carlos", "Mario", "Israel", "Manuel", "Luis"]
+#for y in range(1):
+#    print(nombres[y])
+
+#print(len(nombres))
+
+
+#nombres = ["Paola", "Carlos", "Mario", "Israel", "Manuel", "Luis"]
 
 ## While
 
-i = 0
-while 1 < len(nombres):
-    print(nombres[i])
-    i = i + 1
+#i = 0
+#while 1 < len(nombres):
+#    print(nombres[i])
+#    i = i + 1
+
+#Las listas son estructuras especiales de datos
+
+#se pueden utilizar diferentes métodos
+
+nombres = ["Paola", "Carlos", "Mario", "Israel",
+           "Manuel", "Luis", "Guillermo", "David"]
+
+#Imprime toda lista ordenada de forma ascendente
+# nombres.sort()
+# print(nombres)
+
+# #Imprime por la longitud de la cadena
+# nombres.sort(key=len)
+# print(nombres)
+
+# #Imprime la lista de forma descendente
+# nombres.sort(reverse=True)
+# print(nombres)
+
+# #Imprime por la longitud de la cadena, en froma descendente.
+# nombres.sort(key=len, reverse=True)
+# print(nombres)
+
+nombres = ["Paola", "Carlos", "Mario", "Israel",
+           "Manuel", "Luis", "Guillermo", "David"]
+
+
+# Se hacen copias de la original, se utilizan las copias.
+#Sorted es un método que recibe algo y lo ordena
+alfabetico = sorted(nombres.copy())
+keys = nombres.copy()
+reversas = nombres.copy()
+
+print(alfabetico)
+print(keys)
+print(reversas)
